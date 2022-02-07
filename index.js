a99578379a62d90c01ea60a3fa0866ced78cde05
Cyprien-B <cyprien.b@protonmail.com>
2022-02-07 15:28:14 +0100

    Add btn in JS

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 // Button to hide and display content of configuration exo 1
-
 let btnName = document.getElementById('btnId');
-btnName.addEventListener('click', ()=>{
-  let element = document.getElementById('elementId');
-	if (element.style.display != 'inline'){
-    	element.style.display = 'inline';
-      btnName.innerHTML = "Masquer";
-    }
-  else{
-    element.style.display = 'none'
-    btnName.innerHTML = "Afficher";
-  }
-})
-
-// button to hide and display content get browser exo 6
+let element = document.getElementById('elementId');
 let btnNameExo6 = document.getElementById('btnIdExo6');
-btnNameExo6.addEventListener('click', ()=>{
-  let elementExo6 = document.getElementById('elementIdExo6');
-	if (elementExo6.style.display != 'inline'){
-    elementExo6.style.display = 'inline';
-      btnNameExo6.innerHTML = "Masquer";
-    }
-  else{
-    elementExo6.style.display = 'none'
-    btnNameExo6.innerHTML = "Afficher";
+let elementExo6  = document.getElementById('elementIdExo6');
+
+
+function toHideAndShow(btn, tohide) {
+if (tohide.style.display != 'inline'){
+  tohide.style.display = 'inline';
+  btn.innerHTML = "Masquer";
   }
-})
+else{
+  tohide.style.display = 'none'
+  btn.innerHTML = "Afficher";
+}
+}
+
+btnName.addEventListener('click',() => toHideAndShow(btnName,element))
+btnName.addEventListener('click',() => toHideAndShow(btnNameExo6,elementExo6))
