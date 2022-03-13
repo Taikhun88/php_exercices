1dd3b70b2cee0c9d54208483815c3d4fb1de6abe
Taikhun <taiyang78190@gmail.com>
2022-03-13 15:31:24 +0100

    creation pages related to authentication for user on web and databases, tables user and sessions --DONE

---
+++ authentication/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentification</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/authentication">Page d'accueil'</a>
+        <a href="/authentication/login.php">Se connecter</a>
+        <a href="/authentication/logout.php">Se déconnecter</a>
+        <a href="/authentication/profile.php">Profil</a>
+        <a href="/authentication/register.php">S'inscrire'</a>
+    </nav>
+
+    <h1>Homepage</h1>
+
+</body>
+
+</html>
